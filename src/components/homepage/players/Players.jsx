3fbc18963f79429b24546a7c0a7d31c0bf37583c
Ananya Chakraborty Gourab <aborty174@gmail.com>
2026-03-28 13:35:02 +0600

    working for tate management and toggle

--- src/components/homepage/players/Players.jsx
+++ src/components/homepage/players/Players.jsx
@@ -1,25 +1,57 @@
-import React, { use } from 'react';
-import { AvailablePlayers } from './AvailablePlayers';
+import { use } from 'react'; 
+import { AvailablePlayers } from './AvailablePlayers'; 
+import { selectedPlayers } from './selectedPlayers/selectedPlayers';
+import { useState } from 'react'; 
+
+export const Players = ({ playersPromise }) => { 
+  if (!playersPromise) { 
+    return <div>Loading players...</div>; 
+  } 
+
+  const players = use(playersPromise); 
+
+  const [selectedType,setSelectedType]=useState("available"); 
+  console.log(selectedType,"selectedType") 
+
+  return ( 
+    <div className='container mx-auto my-[60px]'> 
+      players: {players.length} 
 
 
+      <div className='flex justify-between items-center mb-[30px]'> 
+        {selectedType==="available" ? <h2 className="font-bold text-3xl">Available players</h2> : <h2 className='font-bold text-3xl'>Selected player (2/6)</h2>}
 
-export const Players = ({ playersPromise }) => {
-  if (!playersPromise) {
-    // Fallback UI if the promise isn't provided yet
-    return <div>Loading players...</div>;
-  }
+        <div className='flex'> 
+          <button 
+          onClick={()=>setSelectedType("available")}
+            className={`btn ${ 
+              selectedType === "available" ? "bg-[#E7FE29]" : "bg-green-500" 
+            } rounded-r-none rounded-l-2xl`} 
+          > 
+            Available
+          </button> 
 
-  const players = use(playersPromise); // suspends until resolved
+          <button 
+           onClick={()=>setSelectedType("selected")}
+            className={`btn ${ 
+              selectedType === "selected" ? "bg-[#E7FE29]" : "bg-green-500" 
+            } rounded-l-none rounded-r-xl`} 
+          > 
+            Selected(0)
+          </button> 
+        </div> 
+      </div> 
 
-  // Ensure data is always an array
-  //const playersArray = Array.isArray(data) ? data : data.players || [];
-  //console.log(data)
+      
 
-  return (
-    <div className='container mx-auto'>
-      players: {players.length}
-
-     <AvailablePlayers players={players}/>
-    </div>
-  );
+      {/* Cards (now below with gap) */}
+      <div className='mt-[20px]'>
+        {selectedType==="available" ? (<AvailablePlayers players={players}/> 
+      ):(
+     <selectedPlayers />
+      )
+      }
+      </div>
+    </div> 
+  ); 
 };
---
+++ src/components/homepage/players/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const selectedPlayers = () => {
+  return (
+    <div>selectedPlayers</div>
+  )
+}
